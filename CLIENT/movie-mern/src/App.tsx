48a636465be823e0571acd106ef9b8b17e9ebe04
mig-gonz <mig256@outlook.com>
2023-06-19 09:05:17 -0700

    refactored code

--- CLIENT/movie-mern/src/App.tsx
+++ CLIENT/movie-mern/src/App.tsx
@@ -16,9 +16,10 @@ export default function App() {
   const [movies, setMovies] = useState([]); // handles list of movies returned from api
   const [selectedMovie, setSelectedMovie] = useState([]); // handles banner display
 
-  const API_URL = "https://api.themoviedb.org/3/search/movie?api_key=";
-  const FEAT_API_URL = "https://api.themoviedb.org/3/discover/movie?api_key=";
-  const API_KEY = "7b627fa55bf0652f8c45e9da6e8199d1";
+  const API_URL = process.env.REACT_APP_API_URL;
+  const FEAT_API_URL = process.env.REACT_APP_DISCOVER_URL;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
   // const BACKDROP_IMG_PATH = "https://image.tmdb.org/t/p/w1280/";
 
   async function fetchFeaturedMovies() {
